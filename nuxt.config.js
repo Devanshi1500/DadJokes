1cd8c4ba69c118cbb69c5479c67b49ad06e9ddcd
Devanshi Shah <devanshi1500@gmail.com>
2021-11-10 14:04:51 +0530

    changes for deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: 'static',
+  router: {
+    base: '/DadJokes/'
+  },
   head: {
     title: 'dadjokes',
     htmlAttrs: {
